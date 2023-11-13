40e5443df104de468710fa9c73cdb5d6d28defb9
Juan Veliz <92879629+juanveliz76@users.noreply.github.com>
2023-11-12 22:05:14 -0500

    Add files via upload

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,36 +1,41 @@
-using System.Text;
+using System;
+using System.IO;
+using System.Net.Http;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace EsepWebhook;
-
-public class Function
+namespace EsepWebhook
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(object input, ILambdaContext context)
+    public class Function
     {
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-        
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        
-        var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
-        {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
+        private static readonly HttpClient _httpClient = new HttpClient();
 
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
-            
-        return reader.ReadToEnd();
+        public string FunctionHandler(object input, ILambdaContext context)
+        {
+            try
+            {
+                dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+
+                string payload = $"{{\"text\":\"Issue Created: {json.issue.html_url}\"}}";
+
+                var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+                {
+                    Content = new StringContent(payload, System.Text.Encoding.UTF8, "application/json")
+                };
+
+                var response = _httpClient.Send(webRequest);
+                using var reader = new StreamReader(response.Content.ReadAsStream());
+
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception (log it, rethrow it, etc.)
+                Console.WriteLine($"Error: {ex.Message}");
+                throw;
+            }
+        }
     }
 }
